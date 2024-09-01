5f8925a5cf157264650ea5ca849a3d436a8d8024
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-01 21:45:10 +0530

    minor code change

--- Frontend/Team-Collab-Tool/src/components/forgetpass.jsx
+++ Frontend/Team-Collab-Tool/src/components/forgetpass.jsx
@@ -47,7 +47,7 @@ function Forgetpass() {
             Email
           </label>
           <input
-            {...register("Email", { required: true })}
+            {...register("Email")}
             type="email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Email"
