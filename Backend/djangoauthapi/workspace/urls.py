181d8eeb9440d86754a95e56b6cb658a9d02c30f
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-11 15:48:41 +0530

    Backend setup for RBAC

--- Backend/djangoauthapi/workspace/urls.py
+++ Backend/djangoauthapi/workspace/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WorkspaceViewSet, ProjectViewSet,TaskViewSet
+from .views import WorkspaceViewSet, ProjectViewSet,TaskViewSet,UserViewSET
 
 router = DefaultRouter()
 router.register(r'workspaces', WorkspaceViewSet)
 router.register(r'projects', ProjectViewSet)
 router.register(r'tasks', TaskViewSet)
+router.register(r'users', UserViewSET)
 
 urlpatterns = [
     path('', include(router.urls)),
--- Backend/djangoauthapi/workspace/views.py
+++ Backend/djangoauthapi/workspace/views.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets
 from workspace.models import Workspace,Project,Task
 from workspace.serializers import WorkspaceSerializer, ProjectSerializer, WorkspaceDropdownSerializer, TaskSerializer
-from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
 from account.models import User
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.views import APIView
+from account.renderers import UserRenderer
+from account.serializers import UserProfileSerializer
 from django.db.models import Q
+from django.db.models import Count, Case, When
 
 # View for workspace management
 class WorkspaceViewSet(viewsets.ModelViewSet):
@@ -26,6 +30,8 @@ class WorkspaceViewSet(viewsets.ModelViewSet):
         # only returns workspace that belongs to the authenticated user
         user=self.request.user
         # Return workspaces the user owns or is a member of
+        if user.is_admin:
+            return Workspace.objects.all()  # Admins can view all workspaces
         return Workspace.objects.filter(Q(owner=user) | Q(members=user)).distinct()
     
     def perform_create(self,serializer):\
@@ -35,15 +41,17 @@ class WorkspaceViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         # Allow only the owner to update
         workspace = self.get_object()
-        if workspace.owner != self.request.user:
+        if self.request.user.is_admin or workspace.owner == self.request.user:
+            serializer.save()  # Admin or owner can update
+        else:
             raise PermissionDenied("You are not allowed to edit this workspace.")
-        serializer.save()
     
     def perform_destroy(self, instance):
         # Allow only the owner to delete
-        if instance.owner != self.request.user:
+        if self.request.user.is_admin or instance.owner == self.request.user:
+            instance.delete()
+        else: 
             raise PermissionDenied("You are not allowed to delete this workspace.")
-        instance.delete()
 
 # View for Project Management
 class ProjectViewSet(viewsets.ModelViewSet):
@@ -54,6 +62,8 @@ class ProjectViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         # Only return projects within the workspaces owned by the authenticated user
         user = self.request.user
+        if user.is_admin:
+            return Project.objects.all() #admin can view all the projects
         return Project.objects.filter(Q(workspace__owner=user) | Q(workspace__members=user)).distinct()
 
     def perform_create(self, serializer):
@@ -66,14 +76,18 @@ class ProjectViewSet(viewsets.ModelViewSet):
     
     def perform_update(self, serializer):
         project = self.get_object()
-        if project.workspace.owner != self.request.user:
+        if self.request.user.is_admin or project.workspace.owner == self.request.user:
+            serializer.save() #admin or owner can update project
+        else:
             raise PermissionDenied("You are not allowed to edit this project.")
-        serializer.save()
+        
 
     def perform_destroy(self, instance):
-        if instance.workspace.owner != self.request.user:
+        if self.request.user.is_admin or instance.workspace.owner == self.request.user:
+            instance.delete()
+        else:
             raise PermissionDenied("You are not allowed to delete this project.")
-        instance.delete()
+        
         
 
 # View for Task Management
@@ -84,18 +98,25 @@ class TaskViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
+        if user.is_admin:
+            return Task.objects.all()
         # Return tasks within the projects of the workspaces owned by the user
         return Task.objects.filter(Q(project__workspace__owner=user) | Q(assigned_user=user))
 
     def perform_create(self, serializer):
         user = self.request.user
         project = serializer.validated_data['project']
-        
         # Ensure that the user is the owner of the workspace associated with the project
         if project.workspace.owner != user:
             raise PermissionDenied("You are not authorized to create tasks in this project.")
-        
         serializer.save()
+    
+    def perform_destroy(self, instance):
+        if self.request.user.is_admin or instance.project.workspace.owner == self.request.user:
+            instance.delete()
+        else:
+            raise PermissionDenied("You are not allowed to delete this project.")
+        
         
     @action(detail=False, methods=['get'], url_path='workspace-members/(?P<project_id>[^/.]+)')
     def get_workspace_members(self, request, project_id=None):
@@ -103,7 +124,7 @@ class TaskViewSet(viewsets.ModelViewSet):
             project = Project.objects.get(id=project_id)
             workspace = project.workspace
             members = workspace.members.all()
-            member_data = [{'id': member.id, 'name': member.name} for member in members]  # Assuming 'username' is the field for user names
+            member_data = [{'id': member.id, 'name': member.name} for member in members]  
             return Response(member_data)
         except Project.DoesNotExist:
             return Response({"error": "Project not found"}, status=404)
@@ -114,3 +135,28 @@ class TaskViewSet(viewsets.ModelViewSet):
         projects = Project.objects.filter(workspace__owner=user)
         project_data = [{'id': project.id, 'name': project.name} for project in projects]
         return Response(project_data)
+    
+    # task report generation
+    @action(detail=False, methods=['get'], url_path='report',permission_classes=[IsAdminUser])
+    def get_progress_report(self, request):
+        # Get all tasks and calculate completion rates
+        tasks = Task.objects.values('project__name').annotate(
+            total_tasks=Count('id'),
+            completed_tasks=Count(Case(When(status='completed', then=1))),
+            in_progress_tasks=Count(Case(When(status='in_progress', then=1))),
+            pending_tasks=Count(Case(When(status='pending', then=1))),
+        )
+        return Response(tasks)
+    
+# view to fetch user data for Admin
+class UserViewSET(viewsets.ModelViewSet):
+    queryset = User.objects.all()  # Fetch all users;
+    serializer_class = UserProfileSerializer  
+    permission_classes = [IsAdminUser] 
+    
+    def perform_destroy(self, instance):
+        if self.request.user.is_admin:
+            instance.delete()
+        else:
+            raise PermissionDenied("Only admins can delete users.")
+    
