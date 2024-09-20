43d3d2bd3495b64d15703672c67032074dbdd203
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-20 17:06:08 +0530

    vite config

--- Frontend/Team-Collab-Tool/vite.config.js
+++ Frontend/Team-Collab-Tool/vite.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Ensure this is set to 0.0.0.0
+    host: "0.0.0.0", // Ensure this is set to 0.0.0.0
     port: 5173,
   },
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
