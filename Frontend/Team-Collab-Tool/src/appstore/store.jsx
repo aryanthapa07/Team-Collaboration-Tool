f4ddd9a1055c6fd9a8646b03baf439ae943df85a
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-20 20:54:20 +0530

    code refactor

--- Frontend/Team-Collab-Tool/src/appstore/store.jsx
+++ Frontend/Team-Collab-Tool/src/appstore/store.jsx
@@ -1,57 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-// Function to dynamically import APIs
-const importAPIs = async () => {
-  const [
-    { userAuthApi },
-    { workspaceApi },
-    { projectApi },
-    { TaskApi },
-    { userApi },
-  ] = await Promise.all([
-    import("../services/UserAuthApi"),
-    import("../services/WorkspaceApi"),
-    import("../services/ProjectsApi"),
-    import("../services/TasksApi"),
-    import("../services/UserViewApi"),
-  ]);
-
-  return {
-    userAuthApi,
-    workspaceApi,
-    projectApi,
-    TaskApi,
-    userApi,
-  };
-};
-
-// Initialize store inside an async function
-const initStore = async () => {
-  const { userAuthApi, workspaceApi, projectApi, TaskApi, userApi } =
-    await importAPIs();
-
-  const store = configureStore({
-    reducer: {
-      [userAuthApi.reducerPath]: userAuthApi.reducer,
-      [workspaceApi.reducerPath]: workspaceApi.reducer,
-      [projectApi.reducerPath]: projectApi.reducer,
-      [TaskApi.reducerPath]: TaskApi.reducer,
-      [userApi.reducerPath]: userApi.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(
-        userAuthApi.middleware,
-        workspaceApi.middleware,
-        projectApi.middleware,
-        TaskApi.middleware,
-        userApi.middleware
-      ),
-  });
-
-  setupListeners(store.dispatch);
-
-  return store;
-};
-
-// Export the initialized store
-export const store = initStore();
+import { userAuthApi } from "../services/UserAuthApi";
+import { workspaceApi } from "../services/WorkspaceApi";
+import { projectApi } from "../services/ProjectsApi";
+import { TaskApi } from "../services/TasksApi";
+import { userApi } from "../services/UserViewApi";
+export const store = configureStore({
+  reducer: {
+    [userAuthApi.reducerPath]: userAuthApi.reducer,
+    [workspaceApi.reducerPath]: workspaceApi.reducer,
+    [projectApi.reducerPath]: projectApi.reducer,
+    [TaskApi.reducerPath]: TaskApi.reducer,
+    [userApi.reducerPath]: userApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      userAuthApi.middleware,
+      workspaceApi.middleware,
+      projectApi.middleware,
+      TaskApi.middleware,
+      userApi.middleware
+    ),
+});
+setupListeners(store.dispatch);
