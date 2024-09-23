caf427bc8b3380aebbf83562d06742c76fbac6c6
Aryan-Thapa <aryanthapa219@gmail.com>
2024-09-23 13:20:45 +0530

    Secret Key added

--- Backend/djangoauthapi/djangoauthapi/settings.py
+++ Backend/djangoauthapi/djangoauthapi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from utils.aws_utils import get_parameter
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = get_parameter('/backend/SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
